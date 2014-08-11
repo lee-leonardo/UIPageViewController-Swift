9131e9c24ea7169fd575d4f69e1bc348fa38f5c8
lee-leonardo <leoleez@hotmail.com>
2014-08-11 11:33:05 -0700

    Part 1: Created the Project, Storyboarded, and Setup the Child Page Views

---
+++ UIPageViewControllerExample/PageContentViewController.swift
@@ -0,0 +1,27 @@
+//
+//  PageContentViewController.swift
+//  UIPageViewControllerExample
+//
+//  Created by Leonardo Lee on 8/11/14.
+//  Copyright (c) 2014 Leonardo Lee. All rights reserved.
+//
+
+import UIKit
+
+class PageContentViewController: UIViewController {
+	
+	//To have a label for a childVC and a background image for the child VC.
+	@IBOutlet weak var titleLabel: UILabel!
+	@IBOutlet weak var backgroundImageView: UIImageView!
+	
+	//Variables to make a Page Content View Controller work.
+	var pageIndex : Int?
+	var titleText : String?
+	var imagePath : String?
+	
+	override func viewDidLoad() {
+		self.backgroundImageView.image = UIImage(named: self.imagePath)
+		self.titleLabel.text = self.titleText
+	}
+
+}
---
+++ UIPageViewControllerExample/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  UIPageViewControllerExample
+//
+//  Created by Leonardo Lee on 8/11/14.
+//  Copyright (c) 2014 Leonardo Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+	
+	//IBAction to start the tutorial UIPageVC.
+	@IBAction func startWalkthrough(sender: AnyObject) {
+		
+	}
+	
+	
+	
+	
+	
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+	}
+}
+
